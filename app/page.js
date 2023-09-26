6cb2b7457c5793dca21708fec2030c4aad30913e
JIYEON-H <jiyeon.heo0906@gmail.com>
2023-09-26 11:23:42 -0600

    Change styles
    Change styles on app\layout.js, app\page.js, and app\week2\page.js

--- app/page.js
+++ app/page.js
@@ -4,17 +4,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='bg-orange-50'>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <StudentInfo />
-      <ul>
-        <li>
-          <Link href='/week2'>Week 2</Link>
-        </li>
-        <li>
-          <Link href='/week3'>Week 3</Link>
-        </li>
-      </ul>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm'>
+        <h1 className='text-4xl'>CPRG 306: Web Development 2 - Assignments</h1>
+        <div className='mt-6'>
+          <StudentInfo />
+        </div>
+        <ul className='underline underline-offset-1 mt-6'>
+          <li>
+            <Link href='/week2'>Week 2</Link>
+          </li>
+          <li>
+            <Link href='/week3'>Week 3</Link>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
--- app/week2/page.js
+++ app/week2/page.js
@@ -2,10 +2,10 @@ import StudentInfo from "../studentInfo";
 
 export default function MyshoppingsPage() {
   return (
-    <div>
-      <h1>My Shopping List</h1>
+    <div className='flex flex-col items-center justify-between p-24 '>
+      <h1 className='underline underline-offset-8 text-xl'>My Shopping List</h1>
 
-      <div>
+      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm mt-6'>
         <StudentInfo />
       </div>
     </div>
