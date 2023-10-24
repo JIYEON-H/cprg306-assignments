e3b12a06557298937cbc56b4a46432397836cfcd
JIYEON-H <jiyeon.heo0906@gmail.com>
2023-10-23 20:25:15 -0600

    Added suppressHydrationWarning={true} to the body

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-orange-100`}>{children}</body>
+      <body
+        suppressHydrationWarning={true}
+        className={`${inter.className} bg-orange-100`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
